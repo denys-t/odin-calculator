5dc2516dfa56216e8535a3c888b7c97fa2dfbb86
denys-t <tulipadanys@gmail.com>
2022-04-14 10:52:21 +0300

    Created functions for + - * /

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(a,b) {
+    return Math.add(a,b);
+}
+
+function subtract(a,b) {
+    return Math.subtract(a,b);
+}
+
+function multiply(a,b) {
+    return a * b;
+}
+
+function divide(a,b) {
+    return a / b;
+}
