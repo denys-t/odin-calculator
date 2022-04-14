ff142ad4a09b92b6c6bfba8dd8f249f663e1775f
denys-t <tulipadanys@gmail.com>
2022-04-14 12:09:52 +0300

    Added logic. Need to test and fix bugs

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
+const buttons = document.querySelectorAll("button");
+buttons.forEach( button => {
+    button.addEventListener('click', btnClick);
+});
+
+const screen = document.querySelector("#screen");
+
+let number1 = "";
+let number2 = "";
+let operation = "";
+
+function btnClick() {
+    let btnValue = this.textContent;
+
+    if (!isNaN(parseInt(btnValue)) || btnValue === ".") {     
+        if (operation === "") {
+            if (btnValue === "." && number1.includes(".")) {
+                return;
+            } else {
+                number1 += btnValue;
+                showOnScreen(number1);
+            }
+        } else {
+            if (btnValue === "." && number2.includes(".")) {
+                return;
+            } else {
+                number2 += btnValue;
+                showOnScreen(number2);
+            }
+        }
+    } else if (btnValue === "=") {
+        showOnScreen(operate(operation, parseFloat(number1), parseFloat(number2)));
+        operation = "";
+        number1 = "";
+        number2 = "";
+    } else if (btnValue === "C") {
+        operation = "";
+        number1 = "";
+        number2 = "";
+        showOnScreen("");
+    } else {
+        operation = btnValue;
+    }
+}
+
 function operate(operator, a, b) {
     switch (operator) {
         case "+": return add(a,b);
         case "-": return subtract(a,b);
-        case "*": return multiply(a,b);
+        case "x": return multiply(a,b);
         case "/": return divide(a,b);
-        default: return "Error! Unknown operator."
+        default: return "Error!"
     }
 }
 
 function add(a,b) {
-    return Math.add(a,b);
+    return a + b;
 }
 
 function subtract(a,b) {
-    return Math.subtract(a,b);
+    return a - b;
 }
 
 function multiply(a,b) {
@@ -22,4 +67,8 @@ function multiply(a,b) {
 
 function divide(a,b) {
     return a / b;
+}
+
+function showOnScreen(value) {
+    screen.textContent = value;
 }
